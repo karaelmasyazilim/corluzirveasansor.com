3d824bf568e64c58126ee901c87cb4d86fd26a5c
KerimCetinbas <a.kerim.cetinbas@yandex.com>
2021-03-25 21:56:46 +0300

    index

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -40,9 +40,50 @@
 			</button>
 		</div>
 	</header>
-	<main>
-		<slot></slot>
-	</main>
 </div>
 
 
+
+	<main>
+		<slot></slot>
+	</main>
+
+	<div class="w-full h-156">
+		<iframe title="map" src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d24034.00816978646!2d27.7885961!3d41.1508736!3m2!1i1024!2i768!4f13.1!5e0!3m2!1str!2str!4v1539594890165"
+		 frameborder="0" width="100%" height="100%" style="border:0" allowfullscreen=""></iframe>
+	</div>
+
+
+	<footer class="w-full py-28 bg-cover bg-fixed relative" style="background-image: url('/stock_images/bg-3.jpg')">
+		<div class="w-full h-full absolute top-0 left-0 bg-gray-900 opacity-95"></div>
+		<div class="container container-md mx-auto">
+			<div class="grid grid-cols-1 md:grid-cols-3 uppercase text-white font-semibold text-3xl relative gap-12">
+				<div class="flex flex-col text-center">
+					<span> <i class="fa fa-mobile-alt text-gray-300  text-white p-3"></i> Telefon </span>
+					<a href="tel:05353632881" class="text-lg P-3 text-gray-300"> +90 ( 0535 ) 363 28 81 </a>
+				</div>
+
+
+				<div class="flex flex-col text-center">
+					<span> <i class="fas fa-map-marker-alt text-white p-3"></i> Adres </span>
+					<address class="text-lg P-3 text-gray-300">Şeyhsinan mahallesi eski sanayi çarşısı 1.sokak no:9/1 çorlu/tekirdağ</address>
+				</div>
+
+
+
+
+				<div class="flex flex-col text-center">
+					<span> <i class="fa fa-envelope text-white p-3 "></i> E-posta </span>
+					<a href="mailto:corluzirve@gmail.com" class="text-lg P-3 text-gray-300"> corluzirve@gmail.com </a>
+				</div>
+			</div>
+
+			<div class="flex relative text-gray-300  justify-center pt-12">
+				<a href="#facebook"><i class="fab fa-facebook-f fa-3x p-12"></i></a>
+				<a href="#instagram"><i class="fab fa-instagram fa-3x p-12"></i></a>
+				<a href="#twitter"> <i class="fab fa-twitter fa-3x p-12"></i></a>
+			</div>
+
+			<div class="flex relative text-md P-3 text-gray-300 font-semibold justify-center mt-12"><span>çorlu zirve asansör© 2018 tüm hakları saklıdır</span></div>
+		</div>
+	</footer>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -99,16 +99,150 @@
  <div class="container container-sm mx-auto text-white text-center flex flex-col align-middle items-center">
 	<h3 class="text-4xl font-bold "><span class="text-4xl font-bold text-red-500 inline-block">Kaliteyi</span> iyi işçilik, titiz çalışma, kaliteli malzeme, güzel tasarım ve fonksiyonelliğin mükemmel uyumu ile <span class="text-red-500">sağlıyoruz</span></h3>
 	<hr class="border-t-2 border-b-2 pb-1 mt-3 border-red-500 w-1/3">
-	<button class="border-4 px-8 py-4 m-3 font-bold border-white rounded outline-none">Daha fazla bilgi</button>
+		<button class="border-4 px-8 py-4 m-3 font-bold border-white rounded outline-none">Daha fazla bilgi</button>
  </div>
 </div>
 </div>
 
+
+<div class="w-full bg-white py-22">
+	<div class="container container-md mx-auto">
+		<div class="w-full flex flex-col">
+			<h2 class="text-5xl font-extrabold text-center mb-6 mt-12">Hizmetler</h2>
+			<h3 class="text-3xl font-semibold text-center">Bir işi nasıl yaptığımızın özeti</h3>
+			<hr class="border-0 border-b-1 p-3 border-red-500">
+			<div class="grid grid-cols-1  md:grid-cols-3 gap-12">
+				<div class="flex pt-6  justify-center"> 
+				    <div><span class="inline-block p-6 border-4 border-gray-900 rounded-full"><i class="fas fa-drafting-compass fa-2x"></i></span> </div>
+				
+					<ul class="flex flex-col w-64">
+						
+						<li><h4 class="inline-block pl-8 font-extrabold text-2xl">Plan</h4></li>
+						<li class="inline-block pl-8 font-semibold text-md">Tespit</li>
+						<li class="inline-block  pl-8 font-semibold font-bold text-md">Hazırlık</li>
+						<li class="inline-block  pl-8 font-semibold  font-bold text-md">Projelendirme</li>
+					</ul>
+				</div>
+				<div  class="flex pt-6 justify-center">
+					<div><span class="block p-6 border-4 border-gray-900 rounded-full"> <i class="fas fa-money-bill fa-2x"></i> </span></div>
+					
+					<ul class="flex flex-col  w-64">
+						<li><h4 class="pl-8 font-bold text-2xl">Satış & Montaj</h4></li>
+						<li class="inline-block pl-8 font-semibold text-md">Tedarik</li>
+						<li  class="inline-block pl-8 font-semibold text-md">Montaj</li>
+						<li  class="inline-block pl-8 font-semibold text-md">Test</li>
+					</ul>
+				</div>
+				<div  class="flex pt-6 justify-center">
+					<div><span class="block p-6 border-4 border-gray-900 rounded-full"><i class="fab fa-whmcs fa-2x"></i>  </span></div>
+					
+					<ul class="flex flex-col  w-64">
+						<li><h4 class="pl-8 font-bold text-2xl">Teknik destek</h4></li>
+						<li class="inline-block pl-8 font-semibold text-md">Periyodik Bakımlar</li>
+						<li  class="inline-block pl-8 font-semibold text-md">Arıza & Onarım</li>
+						<li  class="inline-block pl-8 font-semibold text-md">Yedek Parça</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div id="carousel" class="w-full h-screen flex items-center relative z-0" style="background-image: url('/stock_images/asansor3.png');">
+	<div id="carousel-cover" class="w-full h-full absolute duration-1000 top-0 left-0 z-0 "></div>
+	<div class="flex relative z-10 w-full h-full items-center text-center">
+		<div class=" w-16 h-16 flex align-middle items-center bg-gray-700 cursor-pointer hover:bg-gray-900 duration-500">
+		
+				<span class="w-full text-gray-300 hover:text-gray-400"><i class="fa fa-chevron-left fa-2x"></i></span>
+		
+		</div>
+		<div class="flex-1">
+			<div class="container container-md mx-auto text-white">
+				
+			<h2 class="text-5xl font-extrabold text-center mb-6 mt-12 text-white">Ürün adı</h2>
+			<p class=" font-semibold text-lg">Lorem Ipsum, dizgi ve baskı endüstrisinde kullanılan mıgır metinlerdir. 
+				Lorem Ipsum, adı bilinmeyen bir matbaacının bir hurufat numune kitabı oluşturmak üzere bir yazı galerisini alarak karıştırdığı 1500'lerden beri endüstri 
+				standardı sahte metinler olarak kullanılmıştır. Beşyüz yıl boyunca varlığını sürdürmekle kalmamış, aynı zamanda pek değişmeden elektronik dizgiye de sıçramıştır.
+				 1960'larda Lorem Ipsum pasajları da içeren Letraset yapraklarının yayınlanması ile ve yakın zamanda Aldus PageMaker gibi Lorem Ipsum sürümleri içeren masaüstü 
+				 yayıncılık yazılımları ile popüler olmuştur.</p>
+				 <button class="border-4 px-8 py-4 m-3 font-bold border-white rounded outline-none rounded">Daha fazla bilgi</button>
+			</div>
+		</div>
+
+
+		<div class=" w-16 h-16 flex align-middle items-center bg-gray-700 cursor-pointer hover:bg-gray-900 duration-500">
+		
+			<span class="w-full text-gray-300 hover:text-gray-400"><i class="fa fa-chevron-right fa-2x"></i></span>
+	
+	</div>
+	</div>
+</div>
+
+<div class="flex w-full h-screen bg-white">
+   <div class="container container-md mx-auto">
+	    <div class="w-full h-full flex flex-col items-center  justify-center ">
+			<h2 class="text-5xl font-extrabold text-center">PORTFÖY</h2>
+			<h3 class="text-3xl font-semibold text-center my-6">Yapmış oldugumuz bazı çalışmalara göz atabilirsiniz</h3>
+			<hr class="border-t-2 mt-3 mb-12 border-red-500 w-full">
+			<div class="w-full  grid grid-cols-1 sm:grid-cols-3">
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				<div class="w-full h-72  p-6 hover:p-0 hover:cursor-pointer duration-1000">
+					<div class="inner-portfoy w-full h-full bg-cover relative" style="background-image: url('/stock_images/elevator3.jpg');">
+						<div  class="portfoy-cover">
+							<h3 class="text-2xl text-white font-extrabold w-full text-center p-6">Çalışma adı</h3>
+							<p class="w-full px-18 text-white">Yüksek malzeme kalitesi, Tecrübeli ustalarımız sayesinde uzun yıllar sorunsuz kullanılabilecek asansörler</p>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+   </div>
+</div>
 <style>
 	.cardEmploye {
 		@apply h-124 bg-red-500 bg-cover bg-center flex flex-col items-center justify-center relative;
 	}
-
 	.cardEmploye > h3 {
 		@apply text-2xl font-bold text-gray-200 z-10;
 	}
@@ -127,4 +261,29 @@
 	.employe-cover{
 		@apply w-full h-full absolute opacity-40 bg-gray-600 z-10;
 	}
+	#carousel-cover {
+		@apply bg-gray-800 bg-opacity-60; 
+	}
+	#carousel {
+		background-repeat: no-repeat;  
+		background-position: 0% 0%;
+		background-size: 100% 100%;
+		-moz-transition: all .5s;
+    	-webkit-transition: all .5s;
+    	transition: all .5s;
+		
+	}
+	#carousel:hover {
+		background-size: 110% 110%;
+
+	}
+	#carousel:hover > #carousel-cover {
+		@apply bg-gray-800 opacity-95 ;
+	}
+
+	.portfoy-cover {
+		@apply h-full w-full absolute top-0 left-0 bg-gray-900 opacity-50 hover:opacity-95 duration-500;
+	}
+
+	
 </style>
